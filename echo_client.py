f2080af98a707545d013e7e99a3352726ff98682
riley <maxm215@gmail.com>
2017-02-17 16:47:47 -0500

    Add client/server wrapper

--- echo_client.py
+++ echo_client.py
@@ -1,29 +1,30 @@
 import socket
-import sys
+import handshake
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('192.168.193.214', 10000)
+ip = input("sever ip address: ")
+server_address = (ip, 10000) #192.168.193.214
 print('connecting to %s port %s' % server_address)
 sock.connect(server_address)
 
-try:
-    
-    # Send data
-    message = b'line four is very very long and stuff and ling anad lng'
-    print('sending "%s"' % message)
-    sock.sendall(message)
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-    
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print('received "%s"' % data)    
+def send_data(message):
+    try:
+        # Send data
+        # message = b'line four is very very long and stuff and ling anad lng'
+        print('sending "%s"' % message)
+        sock.sendall(message)
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
 
-finally:
-    print('closing socket')
-    sock.close()
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            print('received "%s"' % data)
+
+    finally:
+        print('closing socket')
+        sock.close()
