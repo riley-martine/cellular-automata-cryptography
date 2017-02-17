7cc1a898be207a7b19b2f0377122e764e140bc98
riley <riley.martine@protonmail.com>
2017-02-17 17:10:15 -0500

    how about subprocesses

--- server_client_wrapper.py
+++ server_client_wrapper.py
@@ -4,14 +4,15 @@ import socket
 print("Your IP: ", end ='')
 print([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
 
-import threading
+import subprocess
 import client
 import server
 
 
-server_thread = threading.Thread(target=server.start_server())
-server_thread.start()
-server_thread.run()
+# server_thread = threading.Thread(target=server.start_server())
+# server_thread.start()
+# server_thread.run()
+server_sub = subprocess.run(['python3', 'server.py'])
 
 client.connect()
 
