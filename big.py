ae6f4d4a2e8f7d729d5465ccc32d69bc48951e67
Riley Martine <maxm215@gmail.com>
2017-02-21 15:12:48 -0500

    Removed padding

--- big.py
+++ big.py
@@ -6,5 +6,9 @@ from functions import get_rows
 
 rule_dict = RuleList(90).rules
 
-first_row = list('1')
-rows = get_rows(first_row, rule_dict, 500)
+first_row = list('1'*100)
+rows = get_rows(first_row, rule_dict, 5000)
+with open("rows", 'w') as row_file:
+    for row in rows:
+        row_file.write(row)
+        row_file.write("\n")
