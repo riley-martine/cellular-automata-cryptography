719fab03e43f8a330436914618386d935bedffe0
riley <maxm215@gmail.com>
2017-02-23 19:55:38 -0500

    Revamped blocks to work with new cellautocommon

--- blocks.py
+++ blocks.py
@@ -2,26 +2,41 @@
 """Prints largest rule 30 cellular automata terminal will fit."""
 
 import os
-import rules
-from collections import deque
-from functions import get_rows
+from cellautocommon import get_rows
+
+
+BLOCK_RULE_LIST = {
+    ('00', '00'): u' ',
+    ('00', '01'): u'▗',
+    ('00', '10'): u'▖',
+    ('00', '11'): u'▄',
+    ('01', '00'): u'▝',
+    ('01', '01'): u'▐',
+    ('01', '10'): u'▞',
+    ('01', '11'): u'▟',
+    ('10', '00'): u'▘',
+    ('10', '01'): u'▚',
+    ('10', '10'): u'▌',
+    ('10', '11'): u'▙',
+    ('11', '00'): u'▀',
+    ('11', '01'): u'▜',
+    ('11', '10'): u'▛',
+    ('11', '11'): u'█',
+}
 
 
 def blockify(rows):
     """Takes a list of strings of 1s and 0s and returns blocked version."""
-    rows = deque(rows)
-    # Pad to height/len divisible by 4
-    columns_to_pad = len(rows[0]) % 4
-    rows_to_pad = len(rows) % 4
+    # Pad to height/len divisible by 2
+    columns_to_pad = len(rows[0]) % 2
+    rows_to_pad = len(rows) % 2
 
     for _ in range(columns_to_pad):
-        for i in range(len(rows)):
+        for i, row in enumerate(rows):
             rows[i] += '0'
 
     for _ in range(rows_to_pad):
-        rows.extendleft(['0' * len(rows[0])])
-
-    rows = list(rows)  # for easier slicing
+        rows = ['0' * len(rows[0])].extend(rows)
 
     # Grab each four square
     blocked_rows = []
@@ -29,32 +44,32 @@ def blockify(rows):
     for i in range(0, len(rows), 2):  # For every other row
         row = rows[i]
         add_row = ''
-        for n in range(0, len(row), 2):
-            add_row += (rules.BLOCK_RULE_LIST[(rows[i]
-                                               [n:n + 2], rows[i + 1][n:n + 2])])
+        for j in range(0, len(row), 2):
+            add_row += (BLOCK_RULE_LIST[(rows[i]
+                                         [j:j + 2], rows[i + 1][j:j + 2])])
 
         blocked_rows.append(add_row)
 
     return blocked_rows
 
 
-def print_automata(first_row, rule_dict, max_height, max_width):
+def print_automata(first_row: str, rule: int) -> None:
+    """Print 1D automata with first row $first_row, rule $rule.
+       Dimensions are determined by the size of the terminal.
+       Prints largest that will fit using unicode block characters.
+    """
+    # Get maximum size
+    terminal_rows, terminal_columns = os.popen('stty size', 'r').read().split()
+    max_height = (int(terminal_rows) - 2) * 2 # Don't touch these.
+    max_width = (int(terminal_columns) - len(first_row)) * 2
     number_of_rows = int(min(((max_width - len(first_row)) / 2), max_height))
-    rows = get_rows(first_row, rule_dict, number_of_rows)
+
+    rows = get_rows(first_row=first_row, rule=rule, number_of_rows=number_of_rows, pad_rows=True)
 
     # Make readable
     rows = blockify(rows)
     for row in rows:
         print(row)
 
-
-terminal_rows, terminal_columns = os.popen('stty size', 'r').read().split()
-# terminal_rows = (int(terminal_rows) // 4) * 4
-# terminal_columns = (int(terminal_columns) // 4) * 4
-# print(terminal_rows)
-
-
-RULES = rules.RuleList(90).rules
-seed = deque(list('1'))
-print_automata(seed, RULES, (int(terminal_rows) - 2)
-               * 2, (int(terminal_columns) - len(seed)) * 2)
+if __name__ == "__main__":
+    print_automata(first_row='1', rule=30)
