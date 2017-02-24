fdb19a097a55631661cf35c025ebefd16fc12faf
riley <maxm215@gmail.com>
2017-02-23 20:05:40 -0500

    Make print triangles work again

--- print_triangles.py
+++ print_triangles.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 """prints a bunch of random triangles."""
 
-from functions import get_rows
-import rules
-
 import time
 import random
 import os
+from cellautocommon import get_rows
 
-
-def print_automata(first_row, rule_dict, max_height, max_width):
+def print_automaton(first_row, rule):
+    """Print largest 1D automaton that will fit onscreen."""
+    terminal_rows, terminal_columns = os.popen('stty size', 'r').read().split()
+    max_height = (int(terminal_rows) - 1) # Current line
+    max_width = (int(terminal_columns) - len(first_row))
     number_of_rows = int(min(((max_width - len(first_row)) / 2), max_height))
-    rows = get_rows(first_row, rule_dict, number_of_rows)
 
-    # Make readable
+    rows = get_rows(first_row=first_row, rule=rule, number_of_rows=number_of_rows, pad_rows=True)
+
+    # Make easier to view
     for row in rows:
         print(row.replace('1', '█').replace('0', '░'))
 
-    # print(*rows, sep='\n')
-
-for _ in range(10000):
-    terminal_rows, terminal_columns = os.popen('stty size', 'r').read().split()
-    num = random.randint(0, 255)
-    RULES = rules.RuleList(random.randint(0, 255)).rules
-    bin_row = format(num, '08b')
-    print_automata(bin_row, RULES, int(terminal_rows), int(terminal_columns))
-    time.sleep(1)
+if __name__ == "__main__":
+    for _ in range(10000):
+        num = random.randint(0, 255)
+        RULE = random.randint(0, 255)
+        bin_row = format(num, '08b')
+        print_automaton(first_row=bin_row, rule=RULE)
+        time.sleep(1)
